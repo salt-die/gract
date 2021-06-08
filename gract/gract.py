f6338721f0ca37052a4679a6b38c90e9f5fe5c67
salt-die <53280662+salt-die@users.noreply.github.com>
2021-06-08 02:28:16 -0500

    To prevent scheduling nodes' `update_forever` methods multiple times, we added a check.
    `adjacency` property renamde to `nx_adjlist`.

--- gract/gract.py
+++ gract/gract.py
@@ -1,13 +1,19 @@
 from random import choices
 
-from .scheduler import is_running, run_soon
+from .scheduler import run_soon
 
 
+# TODO: gracts shouldn't care if they're node-centric or edge-centric; current implementation is node-centric only.
 class Gract:
-    __slots__ = 'nodes',
+    __slots__ = 'nodes', '_running'
 
     def __init__(self, node_type, neighbors_type, nnodes):
+        self._running = False
         self.nodes = tuple( node_type(neighbors_type) for _ in range(nnodes) )
+
+    @property
+    def is_running(self):
+        return self._running
 
     @classmethod
     def random_graph(cls, node_type, neighbors_type, degree, nnodes):
@@ -19,13 +25,18 @@ class Gract:
         return g
 
     @property
-    def adjacency(self):
-        """Return an adjacency list of the graph in a string format readable by networkx.
+    def nx_adjlist(self):
+        """The current adjacency list of the graph in a string format readable by networkx.
         """
-        return '\n'.join(f'{node.id} {" ".join(str(neighbor.id) for neighbor in node.neighbors)}' for node in self)
+        return '\n'.join(f'{node.id} {" ".join(map(str, node.neighbors))}' for node in self)
 
     def __iter__(self):
         yield from self.nodes
 
     def run(self):
+        if self._running:
+            raise RuntimeError('gract already running')
+
+        self._running = True
+
         run_soon(node.update_forever() for node in self)
