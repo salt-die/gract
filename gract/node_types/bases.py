7380ba200ae795b88fd8f92cab459702ab49412b
salt-die <53280662+salt-die@users.noreply.github.com>
2021-06-09 10:13:24 -0500

    `updates` renamed `activity`.
    `metadata` property added.

--- gract/node_types/bases.py
+++ gract/node_types/bases.py
@@ -26,7 +26,7 @@ class Node(ABC):
     Node implementations require a `neighbors_cls` kwarg in the class definition.
 
     """
-    __slots__ = 'id', 'neighbors', 'updates'
+    __slots__ = 'id', 'neighbors', 'activity'
 
     _nnodes = count()
 
@@ -40,7 +40,7 @@ class Node(ABC):
 
         def __init__(self, neighbors=()):
             self.id = next(self._nnodes)
-            self.updates = 0
+            self.activity = 0
 
             self.neighbors = neighbors_cls(neighbors)
 
@@ -49,6 +49,12 @@ class Node(ABC):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def metadata(self):
+        """Additional node information in a human readable format.
+        """
+        return f'type: {type(self).__name__}'
+
     @abstractmethod
     def update(self):
         """
@@ -56,7 +62,8 @@ class Node(ABC):
 
         Notes
         -----
-        `updates` needs to be incremented by 1 in this method.
+        `activity` should be increased by 1 every time this method is called. (Or when this method is called and doesn't return early.)
+
         """
 
     async def update_forever(self):
