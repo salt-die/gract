b1135c04c5c5e45680d954402a6b297a602bd7a0
salt-die <53280662+salt-die@users.noreply.github.com>
2021-06-09 10:11:19 -0500

    Saving node metadata added.
    Doc stringed added to `save`.

--- gract/analysis/__init__.py
+++ gract/analysis/__init__.py
@@ -1,7 +1,7 @@
 from time import strftime
 from pathlib import Path
 
-import networkx as nx
+import networkx as nx  # MAYBE: Defer this import until needed.
 
 from .histogram import save_histogram
 from .visualization import save_visualization
@@ -10,25 +10,48 @@ RESULTS = 'results'
 HISTOGRAMS = 'histograms'
 VISUALS = 'visuals'
 
-def save(gract, *, histograms=True, visualizations=True):
+def save(gract, **kwargs):
+    """
+    Save polled adajacency lists of a gract.
+
+    Optional Parameters
+    -------------------
+    activity: bool
+        Save node activity if true. (default: False)
+
+    metadata: bool
+        Save node meta-data if true. (default: False)
+
+    histograms: bool
+        Save degree histograms if true. (default: False)
+
+    visualizations: bool
+        Save graph visualizations if true. (default: False)
+
+    """
     root = Path() / RESULTS / strftime('%m_%d_%y__%H_%M_%S')
     root.mkdir()
 
     histogram_path = root / HISTOGRAMS
     visualization_path = root / VISUALS
 
-    if histograms:
+    if histograms := kwargs.get('histograms'):
         histogram_path.mkdir()
 
-    if visualizations:
+    if visualizations := kwargs.get('visualizations'):
         visualization_path.mkdir()
 
-    for i, (adjlist, activity) in enumerate(gract.results):
+    for i, (adjlist, activity, meta_data) in enumerate(gract.results):
         adjlist_path = root / f'{i}.adjlist'
         adjlist_path.write_text(adjlist)
 
-        activity_path = root / f'{i}.activity'
-        activity_path.write_text(activity)
+        if kwargs.get('activity'):
+            activity_path = root / f'{i}.activity'
+            activity_path.write_text(activity)
+
+        if kwargs.get('metadata'):
+            meta_data_path = root / f'{i}.metadata'
+            meta_data_path.write_text(meta_data)
 
         if histograms or visualizations:
             g = nx.read_adjlist(adjlist_path, create_using=nx.MultiDiGraph, nodetype=int)
