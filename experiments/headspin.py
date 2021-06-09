d5eac368ae21b610954487652696c2a721ab2e91
salt-die <53280662+salt-die@users.noreply.github.com>
2021-06-09 09:14:37 -0500

    `delay` is now argument of `run`.

--- experiments/headspin.py
+++ experiments/headspin.py
@@ -2,16 +2,16 @@ from gract import Gract
 from gract.node_types import HeadSpin
 
 # Run experiment
-gract = Gract.random_graph(node_types=HeadSpin, nnodes=1000, poll_delay=10, degree=5)
+gract = Gract.random_graph(node_types=HeadSpin, nnodes=1000, degree=5)
 
-# gract.run(npolls=3) is more efficient, but we're checking that the event-loop is properly destroyed and re-init after each run.
-gract.run(npolls=1)
+# gract.run(npolls=3, delay=10) is more efficient, but we're checking that the event-loop is properly destroyed and re-init after each run.
+gract.run(npolls=1, delay=10)
 print('Polled once')
 
-gract.run(npolls=1)
+gract.run(npolls=1, delay=10)
 print('Polled twice')
 
-gract.run(npolls=1)
+gract.run(npolls=1, delay=10)
 print('Polled thrice')
 
 print('Saving...')
