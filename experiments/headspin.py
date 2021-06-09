a32844630f8329af847612f4f0270902b82d3baa
salt-die <53280662+salt-die@users.noreply.github.com>
2021-06-09 10:24:25 -0500

    `activity=True` required to save activity.

--- experiments/headspin.py
+++ experiments/headspin.py
@@ -3,4 +3,4 @@ from gract.node_types import HeadSpin
 
 gract = Gract.random_graph(node_types=HeadSpin, nnodes=1000, degree=5)
 gract.run(npolls=3, delay=10)
-gract.save(histograms=True, activity=True,)
+gract.save(activity=True, histograms=True, visualizations=True)
